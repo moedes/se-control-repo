be524b971fbc82d3adb31575900ad589c06c6b23
Jerry Mozes <jerry@puppet.com>
2020-08-31 11:10:38 -0600

    Change jerry gid to 1000

--- site-modules/profile/manifests/platform/baseline/users/linux.pp
+++ site-modules/profile/manifests/platform/baseline/users/linux.pp
@@ -3,9 +3,9 @@ class profile::platform::baseline::users::linux {
   user { 'jerry':
     ensure   => 'present',
     comment  => 'Account for Jerry',
-    gid      => '100',
+    gid      => '1000',
     home     => '/home/jerry',
-    password => 'Homerun5001',
+    password => 'puppetftw',
     shell    => '/bin/bash',
     uid      => '1000',
   }
