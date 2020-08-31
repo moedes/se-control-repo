54f516ecd50b0225bf68719f633e323f0385c3ea
Jerry Mozes <jerry@puppet.com>
2020-08-31 14:25:59 -0600

    remove whitespace

--- site-modules/profile/manifests/platform/baseline/users/linux.pp
+++ site-modules/profile/manifests/platform/baseline/users/linux.pp
@@ -12,24 +12,7 @@ class profile::platform::baseline::users::linux {
   ssh_authorized_key { 'jerrymozes@Jerrys-MacBook-Pro.local':
         user => 'docker',
         type => 'ssh-rsa',
-        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDIFgnHvuMFO
-                rJZwi0YXHzbnroPaQQ9GGg8aqYSuGUbkGx8+Y9GhBr
-                b/GAcamxkYXU66E11s1CksGFhlQDfpw56wTYVV33pP
-                LCFE0LygAmyY8MnSlxDRcJmcp7V3g11B5Jt4xopBAa
-                gA5WZC3wx6VRlYM5ub1US51DXJWQTZW6b5ok+lqrrW
-                /Qk4VlLu8wdyKrrBAaTvk421YOltU3gMLfltsZoIH5
-                gt1n3aVLC271Pw9KNKeMndD1KEubfph+9Qtv7Wg5zi
-                BZfiP29KJPRHE9BiWMaTGEPwlMHuGKKAh6ZYggaKAL
-                cnnTAtgtqYDEPxmabZSgb9H6hgJ+cvyz5wSFEzCdul
-                Ji5WziJyfdq5N/btTMUbq9PqpqOsd/e/Yb4alZdLRO
-                qRGsjw6IAiH9kHeG9F3jCl/AXvI/JtygVobb9hkTk1
-                kRnmrMO83DQ8RTaGMZWQqseQXVUW98wWo2yFDOdCKt
-                xGMBY9Fv1hffse6ctR/D6Ohnbgg/dIeLoHV4/Ykme/
-                V6YcD23AfGu+ErhRwYuNS7m9qqobBowkgdfDlfwLN1
-                o6DvmLk+91aWTdVv9XAX66pzkOqrceQ6fCzfRU4IvT
-                RO2SqlQly+NpkMd2VLe38kbT59y2br/EREmamaX+4t
-                kSjMOqq4z4DokXUfvWX3Pmd/d1paLsmlCs06+JwcHh
-                w==',
+        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDIFgnHvuMFOrJZwi0YXHzbnroPaQQ9GGg8aqYSuGUbkGx8+Y9GhBrb/GAcamxkYXU66E11s1CksGFhlQDfpw56wTYVV33pPLCFE0LygAmyY8MnSlxDRcJmcp7V3g11B5Jt4xopBAagA5WZC3wx6VRlYM5ub1US51DXJWQTZW6b5ok+lqrrW/Qk4VlLu8wdyKrrBAaTvk421YOltU3gMLfltsZoIH5gt1n3aVLC271Pw9KNKeMndD1KEubfph+9Qtv7Wg5ziBZfiP29KJPRHE9BiWMaTGEPwlMHuGKKAh6ZYggaKALcnnTAtgtqYDEPxmabZSgb9H6hgJ+cvyz5wSFEzCdulJi5WziJyfdq5N/btTMUbq9PqpqOsd/e/Yb4alZdLROqRGsjw6IAiH9kHeG9F3jCl/AXvI/JtygVobb9hkTk1kRnmrMO83DQ8RTaGMZWQqseQXVUW98wWo2yFDOdCKtxGMBY9Fv1hffse6ctR/D6Ohnbgg/dIeLoHV4/Ykme/V6YcD23AfGu+ErhRwYuNS7m9qqobBowkgdfDlfwLN1o6DvmLk+91aWTdVv9XAX66pzkOqrceQ6fCzfRU4IvTRO2SqlQly+NpkMd2VLe38kbT59y2br/EREmamaX+4tkSjMOqq4z4DokXUfvWX3Pmd/d1paLsmlCs06+JwcHhw==',
   }
 
 }
