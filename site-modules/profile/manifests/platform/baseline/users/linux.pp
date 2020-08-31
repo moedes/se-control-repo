03ad207dcce7203012f6f0e01471eee605885e2c
Jerry Mozes <jerry@puppet.com>
2020-08-31 14:10:38 -0600

    New pub key

--- site-modules/profile/manifests/platform/baseline/users/linux.pp
+++ site-modules/profile/manifests/platform/baseline/users/linux.pp
@@ -1,10 +1,10 @@
 class profile::platform::baseline::users::linux {
 
-  user { 'mozes':
+  user { 'docker':
     ensure   => 'present',
-    comment  => 'Account for mozes',
+    comment  => 'Account for docker',
     gid      => '100',
-    home     => '/home/mozes',
+    home     => '/home/docker',
     shell    => '/bin/bash',
     uid      => '1010',
   }
@@ -12,16 +12,24 @@ class profile::platform::baseline::users::linux {
   ssh_authorized_key { 'jerry@centostemp':
         user => 'mozes',
         type => 'ssh-rsa',
-        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDLcQpvpL10
-                zvsJcahGGnemqPu81rkaJtmC61uxPppWpxorCBCWC
-                /Ld+MhFwQg/bCUeDkmY4PavntgirBe9yRTpdHtGVz
-                u5chDaF5TFVgyvkbKnm8TlI9WijP5ZkirSUj5IVT8
-                7Fjufggu6zy/g0Rf3yxgkRFu/hyneV8Vaf6mfroFJ
-                9kElNdllEaQurXvHAtcW4a4DQmlX6BvbzymLFilud
-                YY5375qjPShfGTJFFLvTqCur23MlJyD/hYIEzDMZT
-                zcMD2LrwEMoIPcJHNL3LfppZWJfKpgMsm8dJaNYbX
-                T6ac9DYAVEmmwfBr7CEfTNMVynDJuqFMCwNmW4fp6
-                xp7B',
+        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDIFgnHvuMFO
+                rJZwi0YXHzbnroPaQQ9GGg8aqYSuGUbkGx8+Y9GhBr
+                b/GAcamxkYXU66E11s1CksGFhlQDfpw56wTYVV33pP
+                LCFE0LygAmyY8MnSlxDRcJmcp7V3g11B5Jt4xopBAa
+                gA5WZC3wx6VRlYM5ub1US51DXJWQTZW6b5ok+lqrrW
+                /Qk4VlLu8wdyKrrBAaTvk421YOltU3gMLfltsZoIH5
+                gt1n3aVLC271Pw9KNKeMndD1KEubfph+9Qtv7Wg5zi
+                BZfiP29KJPRHE9BiWMaTGEPwlMHuGKKAh6ZYggaKAL
+                cnnTAtgtqYDEPxmabZSgb9H6hgJ+cvyz5wSFEzCdul
+                Ji5WziJyfdq5N/btTMUbq9PqpqOsd/e/Yb4alZdLRO
+                qRGsjw6IAiH9kHeG9F3jCl/AXvI/JtygVobb9hkTk1
+                kRnmrMO83DQ8RTaGMZWQqseQXVUW98wWo2yFDOdCKt
+                xGMBY9Fv1hffse6ctR/D6Ohnbgg/dIeLoHV4/Ykme/
+                V6YcD23AfGu+ErhRwYuNS7m9qqobBowkgdfDlfwLN1
+                o6DvmLk+91aWTdVv9XAX66pzkOqrceQ6fCzfRU4IvT
+                RO2SqlQly+NpkMd2VLe38kbT59y2br/EREmamaX+4t
+                kSjMOqq4z4DokXUfvWX3Pmd/d1paLsmlCs06+JwcHh
+                w==',
   }
 
 }
