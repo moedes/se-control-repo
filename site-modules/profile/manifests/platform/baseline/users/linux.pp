a63e3e75aada480f6250364dc2c9fe8309df8c3b
Jerry Mozes <jerry@puppet.com>
2020-08-31 13:36:29 -0600

    Align public key

--- site-modules/profile/manifests/platform/baseline/users/linux.pp
+++ site-modules/profile/manifests/platform/baseline/users/linux.pp
@@ -10,9 +10,20 @@ class profile::platform::baseline::users::linux {
   }
 
   ssh_authorized_key { 'jerry@pmaster01':
-    user => 'jerry',
-    type => 'ssh-rsa',
-    key => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDW94SfVhhk1usr7mNUJ/YxkXIdKrILvLkYyylVAD9PzpHVrRYiWYjnjnWP9oSzSvx003Yvl24HWXm2Lk5Q9Liskxe+9WyPbM3HRi1wgkiV2ITzJ5781tCPr8msYYF4U4fHh2M+0mSsfvdWfsnqEUmN/utRpwpAmVMFSwepw619sxITIMvqHGHvaPOGioG1UeIIo5XJDnXhbPqDQnhvvBkpO2DsCTPpYF9/8RMU5yGoo8ASs1heLbCbYzMq+3ARfg+ph9hcU7ZCnhNVMCnwcFTDGMzYU78mY+ci2EbsnG+cpFcGh2uGjhTnhH41E6gPDA0KDbDgcVfnGINnESDBsmnDs5YyJbGpZLiHniAvcndHCsTZPOV8/nEEysuaFggkEDIZaYWriV3B1IhnV/wA9xNtZ4WFKgxCp9lL4RUBzi31xFFdMi8sY0kO9xLErC1K5vaSrJdMQHoBJ0lvwyJP2VaPDgcANuk729nzPYdC/2f2twOdjuEXZAFMFu0cw+DRe7hx3xjMvaWLHIjIiiGcXOtBbh8mJTRgXYBKYK5LloeWwgG1o5wXai56XUVuuPiCAE5F7SkNYYIcH578DILPuKz8+nxuHfPIpr9ZT8e/EqYfN3BZij0IrX6BkfAWK9zkcuCLtRw6f2vVYTgb14A3Yf9oEEJ9rX+KnMbq1/VeO6RBbQ==',
+        user => 'jerry',
+        type => 'ssh-rsa',
+        key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDW94SfVhhk1usr7mNUJ/YxkXIdKrI
+                LvLkYyylVAD9PzpHVrRYiWYjnjnWP9oSzSvx003Yvl24HWXm2Lk5Q9Liskxe
+                +9WyPbM3HRi1wgkiV2ITzJ5781tCPr8msYYF4U4fHh2M+0mSsfvdWfsnqEUmN
+                /utRpwpAmVMFSwepw619sxITIMvqHGHvaPOGioG1UeIIo5XJDnXhbPqDQnhvv
+                BkpO2DsCTPpYF9/8RMU5yGoo8ASs1heLbCbYzMq+3ARfg+ph9hcU7ZCnhNVMC
+                nwcFTDGMzYU78mY+ci2EbsnG+cpFcGh2uGjhTnhH41E6gPDA0KDbDgcVfnGIN
+                nESDBsmnDs5YyJbGpZLiHniAvcndHCsTZPOV8/nEEysuaFggkEDIZaYWriV3B
+                1IhnV/wA9xNtZ4WFKgxCp9lL4RUBzi31xFFdMi8sY0kO9xLErC1K5vaSrJdMQ
+                HoBJ0lvwyJP2VaPDgcANuk729nzPYdC/2f2twOdjuEXZAFMFu0cw+DRe7hx3x
+                jMvaWLHIjIiiGcXOtBbh8mJTRgXYBKYK5LloeWwgG1o5wXai56XUVuuPiCAE5
+                F7SkNYYIcH578DILPuKz8+nxuHfPIpr9ZT8e/EqYfN3BZij0IrX6BkfAWK9zk
+                cuCLtRw6f2vVYTgb14A3Yf9oEEJ9rX+KnMbq1/VeO6RBbQ==',
   }
 
 }
