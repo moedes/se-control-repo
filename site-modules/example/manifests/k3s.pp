680ef3cb17cf8919a4305b5e6a7bf0f051dfb9c3
Jerry Mozes <jerry@puppet.com>
2020-09-15 10:54:03 -0600

    add rancher rpm

--- site-modules/example/manifests/k3s.pp
+++ site-modules/example/manifests/k3s.pp
@@ -10,6 +10,11 @@ class example::k3s {
     provider => 'yum'
   }
 
+  package { 'https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm' :
+    ensure => present,
+    provider => 'rpm'
+  }
+
   exec { 'curl' :
     command => 'curl -sfL https://get.k3s.io | sh -',
   }
