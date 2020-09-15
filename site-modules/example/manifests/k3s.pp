823f06edd914094ef872cda33a7ede754888d1b0
Jerry Mozes <jerry@puppet.com>
2020-09-15 11:11:28 -0600

    add path to curl command

--- site-modules/example/manifests/k3s.pp
+++ site-modules/example/manifests/k3s.pp
@@ -3,23 +3,24 @@ class example::k3s {
   package { 'container-selinux' :
     ensure   => present,
     provider => 'yum',
-    before   => Exec['/usr/bin/curl']
+    before   => Exec['curl']
   }
 
   package { 'selinux-policy-base' :
     ensure   => present,
     provider => 'yum',
-    before   => Exec['/usr/bin/curl']
+    before   => Exec['curl']
   }
 
   package { 'https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm' :
     ensure   => present,
     provider => 'rpm',
-    before   => Exec['/usr/bin/curl']
+    before   => Exec['curl']
   }
 
-  exec { '/usr/bin/curl' :
+  exec { 'curl' :
     command => 'curl -sfL https://get.k3s.io | sh -',
+    path    => '/usr/bin'
   }
 
 }
