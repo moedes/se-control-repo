41cfd60434f68be5fc0d384405cb346056f9c2f1
Jerry Mozes <jerry@puppet.com>
2020-09-15 10:57:52 -0600

    add before statements

--- site-modules/example/manifests/k3s.pp
+++ site-modules/example/manifests/k3s.pp
@@ -1,18 +1,21 @@
 class example::k3s {
 
   package { 'container-selinux' :
-    ensure => present,
-    provider => 'yum'
+    ensure   => present,
+    provider => 'yum',
+    before   => Exec['curl']
   }
 
   package { 'selinux-policy-base' :
-    ensure => present,
-    provider => 'yum'
+    ensure   => present,
+    provider => 'yum',
+    before   => Exec['curl']
   }
 
   package { 'https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm' :
-    ensure => present,
-    provider => 'rpm'
+    ensure   => present,
+    provider => 'rpm',
+    before   => Exec['curl']
   }
 
   exec { 'curl' :
