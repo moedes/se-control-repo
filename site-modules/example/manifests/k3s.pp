818b66fc81517e2500036f0f104c6a29ed9e5c52
Jerry Mozes <jerry@puppet.com>
2020-09-15 11:06:27 -0600

    change before hashes

--- site-modules/example/manifests/k3s.pp
+++ site-modules/example/manifests/k3s.pp
@@ -3,19 +3,19 @@ class example::k3s {
   package { 'container-selinux' :
     ensure   => present,
     provider => 'yum',
-    before   => Exec['curl']
+    before   => Exec['/usr/bin/curl']
   }
 
   package { 'selinux-policy-base' :
     ensure   => present,
     provider => 'yum',
-    before   => Exec['curl']
+    before   => Exec['/usr/bin/curl']
   }
 
   package { 'https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm' :
     ensure   => present,
     provider => 'rpm',
-    before   => Exec['curl']
+    before   => Exec['/usr/bin/curl']
   }
 
   exec { '/usr/bin/curl' :
