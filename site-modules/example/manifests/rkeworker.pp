b95c1437c4b9e0eb6e2c2e65f668925102706323
Jerry Mozes <jerry@puppet.com>
2020-09-29 15:57:37 -0600

    updated version of rke to rc9

--- site-modules/example/manifests/rkeworker.pp
+++ site-modules/example/manifests/rkeworker.pp
@@ -3,7 +3,7 @@ class example::rkeworker {
     file {'rke':
         ensure => present,
         path   => '/usr/bin/rke',
-        source => 'https://github.com/rancher/rke/releases/download/v1.1.7/rke_linux-amd64',
+        source => 'https://github.com/rancher/rke/releases/download/v1.2.0-rc9/rke_linux-amd64',
         mode   => '+x'
     }
 
