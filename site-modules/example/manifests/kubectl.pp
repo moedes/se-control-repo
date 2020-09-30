689c33bf0f47d4cf69942195d001d55e5427e163
Jerry Mozes <jerry@puppet.com>
2020-09-30 12:58:40 -0600

    removed curl from the source hash

--- site-modules/example/manifests/kubectl.pp
+++ site-modules/example/manifests/kubectl.pp
@@ -3,7 +3,7 @@ class example::kubectl {
   file {'kubectl':
     ensure => file,
     path      => '/usr/bin/kubectl',
-    source    => 'curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl',
+    source    => 'https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl',
     mode      => '+x',
   }
 }
