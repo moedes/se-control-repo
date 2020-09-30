f52c8f335ce342212f9016e4f544fb52975983a2
Jerry Mozes <jerry@puppet.com>
2020-09-30 09:36:05 -0600

    created checksum_value

--- site-modules/example/manifests/rkeworker.pp
+++ site-modules/example/manifests/rkeworker.pp
@@ -1,11 +1,11 @@
 class example::rkeworker {
 
     file {'rke':
-        ensure       => file,
-        path         => '/usr/bin/rke',
-        source       => 'https://github.com/rancher/rke/releases/download/v1.1.7/rke_linux-amd64',
-        mode         => '+x',
-        checksum     => md5
+        ensure             => file,
+        path               => '/usr/bin/rke',
+        source             => 'https://github.com/rancher/rke/releases/download/v1.1.7/rke_linux-amd64',
+        mode               => '+x',
+        checksum_value     => md5,
     }
 
 }
