e01c0138c0a05d54b20b898a34145c1d2cd45a9e
Jerry Mozes <jerry@puppet.com>
2020-09-30 11:56:32 -0600

    init terraform install

---
+++ site-modules/example/manifests/terraforminst.pp
@@ -0,0 +1,21 @@
+class example::terraforminst {
+  include 'archive'
+
+  package { 'unzip':
+    ensure => 'present',
+  }
+
+  archive { '/usr/bin/terraform':
+    ensure  => present,
+    extract => true,
+    extract_path => '/usr/bin',
+    source  => 'https://releases.hashicorp.com/terraform/0.13.3/terraform_0.13.3_linux_amd64.zip',
+    checksum => '35c662be9d32d38815cde5fa4c9fa61a3b7f39952ecd50ebf92fd1b2ddd6109b',
+    checksum_type => 'sha256',
+    creates => '/usr/bin/terraform',
+    cleanup => true,
+    require => Package['unzip'],
+  }
+
+
+}
