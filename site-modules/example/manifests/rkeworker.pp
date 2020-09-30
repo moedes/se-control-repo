a877b8c9189ee0219ef7c9b5567cde082407ccab
Jerry Mozes <jerry@puppet.com>
2020-09-30 09:48:26 -0600

    add checksum value

--- site-modules/example/manifests/rkeworker.pp
+++ site-modules/example/manifests/rkeworker.pp
@@ -1,11 +1,12 @@
 class example::rkeworker {
 
     file {'rke':
-        ensure             => file,
-        path               => '/usr/bin/rke',
-        source             => 'https://github.com/rancher/rke/releases/download/v1.1.7/rke_linux-amd64',
-        mode               => '+x',
-        checksum_value     => 'md5',
+        ensure    => file,
+        path      => '/usr/bin/rke',
+        source    => 'https://github.com/rancher/rke/releases/download/v1.1.7/rke_linux-amd64',
+        mode      => '+x',
+        checksum  => 'sha256',
+        checksum_value => 'ebc62233f213762ef9686ac9029638f646269d17843ed7b0e4dbd9df5921b855'
     }
 
 }
