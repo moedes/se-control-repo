987e213f6c7038cb8af2b7f44ce729d9bbb6ebd6
Jerry Mozes <jerry@puppet.com>
2020-09-30 09:02:50 -0600

    add sourceselect

--- site-modules/example/manifests/rkeworker.pp
+++ site-modules/example/manifests/rkeworker.pp
@@ -1,10 +1,11 @@
 class example::rkeworker {
 
     file {'rke':
-        ensure => file,
-        path   => '/usr/bin/rke',
-        source => 'https://github.com/rancher/rke/releases/download/v1.1.8-rc1/rke_linux-amd64',
-        mode   => '+x'
+        ensure       => file,
+        path         => '/usr/bin/rke',
+        source       => 'https://github.com/rancher/rke/releases/download/v1.1.8-rc1/rke_linux-amd64',
+        sourceselect => 'all',
+        mode         => '+x'
     }
 
 }
