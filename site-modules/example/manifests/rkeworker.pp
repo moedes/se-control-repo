ce1856447cd0168f29707208594eb17a6d8d0a4f
Jerry Mozes <jerry@puppet.com>
2020-09-30 08:57:15 -0600

    change to version 1.1.8 of source file

--- site-modules/example/manifests/rkeworker.pp
+++ site-modules/example/manifests/rkeworker.pp
@@ -3,7 +3,7 @@ class example::rkeworker {
     file {'rke':
         ensure => file,
         path   => '/usr/bin/rke',
-        source => 'https://github.com/rancher/rke/releases/download/v1.2.0-rc9/rke_linux-amd64',
+        source => 'https://github.com/rancher/rke/releases/download/v1.1.8-rc1/rke_linux-amd64',
         mode   => '+x'
     }
 
