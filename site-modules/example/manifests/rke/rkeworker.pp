631dc902f210298de78efe1a8d31bf1767646850
Jerry Mozes <jerry@puppet.com>
2020-10-26 14:28:37 -0600

    Update to stable release of rke 1.2.1

--- site-modules/example/manifests/rke/rkeworker.pp
+++ site-modules/example/manifests/rke/rkeworker.pp
@@ -3,7 +3,7 @@ class example::rke::rkeworker {
     file {'rke':
         ensure    => file,
         path      => '/usr/bin/rke',
-        source    => 'https://github.com/rancher/rke/releases/download/v1.2.0-rc9/rke_linux-amd64',
+        source    => 'https://github.com/rancher/rke/releases/download/v1.2.1/rke_linux-amd64',
         mode      => '+x',
         checksum  => 'sha256',
         checksum_value => '3ea5099c0b11883980734a104d3f5699c7d82be19849d016116b6ef0fba78ac4'
