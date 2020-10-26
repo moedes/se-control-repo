6b0efc16cfa519f9a72256f76a78484581c7597f
Jerry Mozes <jerry@puppet.com>
2020-10-26 14:32:11 -0600

    Update to checksum value of 1.2.1

--- site-modules/example/manifests/rke/rkeworker.pp
+++ site-modules/example/manifests/rke/rkeworker.pp
@@ -6,7 +6,7 @@ class example::rke::rkeworker {
         source    => 'https://github.com/rancher/rke/releases/download/v1.2.1/rke_linux-amd64',
         mode      => '+x',
         checksum  => 'sha256',
-        checksum_value => '3ea5099c0b11883980734a104d3f5699c7d82be19849d016116b6ef0fba78ac4'
+        checksum_value => 'e1e288d557cea6140f16ba6659fe427fb7431c3cd62cc67f2cdfdf415a4012da'
     }
 
 }
