f58aa9d77b4ffa24a3f2a1a2bf015ff3efd263b5
Jerry Mozes <jerry@puppet.com>
2020-10-29 15:51:57 -0600

    change cafile

--- site-modules/profile/manifests/puppet/cafile.pp
+++ site-modules/profile/manifests/puppet/cafile.pp
@@ -8,7 +8,7 @@ class profile::puppet::cafile (
     ensure => 'directory'
   }
 
-  file {$ssldirectory + '/' + $certfile :
+  file { $ssldirectory/$certfile :
     ensure  => file,
     source  => $source,
     require => File[$ssldirectory]
